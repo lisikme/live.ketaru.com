d91e0b8ee6f86231c57283926dc11a5b55f821e4
KetaruCeatik <87075354+ketaru@users.noreply.github.com>
2023-09-24 07:47:35 +0300

    Update ver2.js

--- ver2.js
+++ ver2.js
@@ -52,112 +52,139 @@ $(function () {
       "FlyFox",
       "https://m.ketaru.com:8000/ketaruweb",
       "img/golive.png",
+      "Играет (Только в Discord)",
       
       "TNT Radio",
       "https://tntradio.hostingradio.ru:8027/tntradio128.mp3",
       "img/tnt.png",
+      "Играет",
       
       "Remix FM",
       "https://rmx.amgradio.ru/RemixFM",
       "img/remix.png",
+      "Играет",
       
       "DFM",
       "https://dfm.hostingradio.ru/dfm128.mp3",
       "img/dfm.png",
+      "Играет",
       
       "Like FM",
       "https://pub0301.101.ru:8443/stream/air/mp3/256/219",
       "img/like.png",
+      "Играет",
       
       "Хайп FM",
       "https://hfm.amgradio.ru/HypeFM",
       "img/hype.png",
+      "Играет",
       
       "Love Radio",
       "https://www.liveradio.es/dfmsaransk.sv-veter.ru:8095/love_saransk?128",
       "img/love.png",
+      "Играет",
       
       "EuropaPlus",
       "https://ep256.hostingradio.ru:8052/europaplus256.mp3",
       "img/ep.png",
+      "Играет",
       
       "EuropaPlus New",
       "https://europaplus.hostingradio.ru:8014/ep-new128.mp3",
       "img/ep_new.png",
+      "Играет",
       
       "EuropaPlus Lisht",
       "https://europaplus.hostingradio.ru:8014/ep-light128.mp3",
       "img/ep_light.png",
+      "Играет",
       
       "EuropaPlus Top40",
       "https://europaplus.hostingradio.ru:8014/ep-top256.mp3",
       "img/ep_top40.png",
+      "Играет",
       
       "Свежее радио",
       "https://epdop.hostingradio.ru:8033/fresh64.aac",
       "img/ep_fresh.png",
+      "Играет",
       
       "Хит FM",
       "https://hitfm.hostingradio.ru/hitfm96.aacp",
       "img/hit.png",
+      "Играет",
       
       "Energy FM",
       "https://pub0102.101.ru:8443/stream/air/mp3/256/99",
       "img/nrg.png",
+      "Играет",
       
       // "Radmir FM",
       // "http://listen7.myradio24.com/nazarik",
       // "img/radmir.png",
+      // "Играет",
       
       "МУЗ Радио",
       "https://online-3.gkvr.ru:8001/muztvradio_original_128.mp3", 
       "img/muz.png",
+      "Играет",
       
       "Радио SRP",
       "https://a7.radioheart.ru:8066/RH6629",
       "img/srp.png",
+      "Играет",
       
       "Relax FM",
       "https://pub0301.101.ru:8443/stream/air/mp3/256/200",
       "img/relax.png",
+      "Играет",
       
       "Русское Радио",
       "https://rusradio.hostingradio.ru/rusradio96.aacp",
       "img/rus.png",
+      "Играет",
       
       "Юмор FM",
       "https://ic5.101.ru:8000/v5_1",
       "img/umor.png",
+      "Играет",
       
       "Новое радио",
       "https://stream.newradio.ru/novoe96.aacp",
       "img/newradio.png",
+      "Играет",
       
       "Radio Record",
       "https://radiorecord.hostingradio.ru/rr_main96.aacp",
       "img/record.png",
+      "Играет",
       
       "Radio Record Phonk",
       "https://radiorecord.hostingradio.ru/phonk96.aacp",
       "img/record_phonk.png",
+      "Играет",
       
       "Radio Record Remix",
       "https://radiorecord.hostingradio.ru/rmx96.aacp",
       "img/record_remix.png",
+      "Играет",
       
       "Radio Record Trap",
       "https://radiorecord.hostingradio.ru/trap96.aacp",
       "img/record_trap.png",
+      "Играет",
       
       "Radio Record Lo-Fi",
       "https://radiorecord.hostingradio.ru/lofi96.aacp",
       "img/record_lofi.png",
+      "Играет",
       
       "Radio Record Rock",
       "https://radiorecord.hostingradio.ru/rock96.aacp",
       "img/record_rock.png",
+      "Играет",
     ],
-    playPreviousTrackButton=$("#play-previous"),playNextTrackButton=$("#play-next"),currIndex=-1;
+    playPreviousTrackButton=$("#play-previous"),playNextTrackButton=$("#play-next"),currIndex=-4;
 
   function playPause() {
     setTimeout(function () {
@@ -232,11 +259,13 @@ $(function () {
       }
       seekBar.width(0);
       trackTime.removeClass("active");
-      currAlbum = albums[currIndex];
-      currImage = './'+active[currIndex];
-      // currTrackName = trackNames[currIndex];
-      currTrackName = 'Играет';
-      audio.src = trackUrl[currIndex];
+      
+      currAlbum = fm_list[currIndex]; // Radio Name
+      audio.src = fm_list[currIndex]+1; // Radio Server
+      currImage = './'+fm_list[currIndex]+2; // Radio Image
+      currTrackName = fm_list[currIndex]+3; // Radio Status
+      
+      
       nTime = 0;
       bTime = new Date();
       bTime = bTime.getTime();
