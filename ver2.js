80678d391910ee9873afb043907326b06874dd40
KetaruCeatik <87075354+ketaru@users.noreply.github.com>
2023-09-24 12:38:10 +0300

    Update ver2.js

--- ver2.js
+++ ver2.js
@@ -249,11 +249,7 @@ $(function () {
     }, 3000);
   }
   function selectTrack(flag) {
-    if (flag == 0 || flag == 1) {
-      ++currIndex;
-      ++currIndex;
-      ++currIndex;
-    }
+    if (flag == 0 || flag == 1) ++currIndex;
     else --currIndex;
     if (currIndex > -1 && currIndex < fm_list.length) {
       if (flag == 0) i.attr("class", "fa fa-play");
