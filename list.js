478edfe7010f6a4823f5e3131fb416499967500e
KetaruCeatik <87075354+ketaru@users.noreply.github.com>
2023-09-27 15:07:07 +0300

    Add files via upload

---
+++ list.js
@@ -0,0 +1,136 @@
+fm_list=[ // Название Трека
+"FlyFox",
+"https://m.ketaru.com:8000/ketaruweb",
+"golive.png",
+"Играет (Только в Discord)",
+
+"TNT Radio",
+"https://tntradio.hostingradio.ru:8027/tntradio128.mp3",
+"tnt.png",
+"Играет",
+
+"Remix FM",
+"https://rmx.amgradio.ru/RemixFM",
+"remix.png",
+"Играет",
+
+"DFM",
+"https://dfm.hostingradio.ru/dfm128.mp3",
+"dfm.png",
+"Играет",
+
+"Like FM",
+"https://pub0301.101.ru:8443/stream/air/mp3/256/219",
+"like.png",
+"Играет",
+
+"Хайп FM",
+"https://hfm.amgradio.ru/HypeFM",
+"hype.png",
+"Играет",
+
+"Love Radio",
+"https://www.liveradio.es/dfmsaransk.sv-veter.ru:8095/love_saransk?128",
+"love.png",
+"Играет",
+
+"EuropaPlus",
+"https://ep256.hostingradio.ru:8052/europaplus256.mp3",
+"ep.png",
+"Играет",
+
+"EuropaPlus New",
+"https://europaplus.hostingradio.ru:8014/ep-new128.mp3",
+"ep_new.png",
+"Играет",
+
+"EuropaPlus Lisht",
+"https://europaplus.hostingradio.ru:8014/ep-light128.mp3",
+"ep_light.png",
+"Играет",
+
+"EuropaPlus Top40",
+"https://europaplus.hostingradio.ru:8014/ep-top256.mp3",
+"ep_top40.png",
+"Играет",
+
+"Свежее радио",
+"https://epdop.hostingradio.ru:8033/fresh64.aac",
+"ep_fresh.png",
+"Играет",
+
+"Хит FM",
+"https://hitfm.hostingradio.ru/hitfm96.aacp",
+"hit.png",
+"Играет",
+
+"Energy FM",
+"https://pub0102.101.ru:8443/stream/air/mp3/256/99",
+"nrg.png",
+"Играет",
+
+// "Radmir FM",
+// "http://listen7.myradio24.com/nazarik",
+// "radmir.png",
+// "Играет",
+
+"МУЗ Радио",
+"https://online-3.gkvr.ru:8001/muztvradio_original_128.mp3", 
+"muz.png",
+"Играет",
+
+"Радио SRP",
+"https://a7.radioheart.ru:8066/RH6629",
+"srp.png",
+"Играет",
+
+"Relax FM",
+"https://pub0301.101.ru:8443/stream/air/mp3/256/200",
+"relax.png",
+"Играет",
+
+"Русское Радио",
+"https://rusradio.hostingradio.ru/rusradio96.aacp",
+"rus.png",
+"Играет",
+
+"Юмор FM",
+"https://ic5.101.ru:8000/v5_1",
+"umor.png",
+"Играет",
+
+"Новое радио",
+"https://stream.newradio.ru/novoe96.aacp",
+"newradio.png",
+"Играет",
+
+"Radio Record",
+"https://radiorecord.hostingradio.ru/rr_main96.aacp",
+"record.png",
+"Играет",
+
+"Radio Record Phonk",
+"https://radiorecord.hostingradio.ru/phonk96.aacp",
+"record_phonk.png",
+"Играет",
+
+"Radio Record Remix",
+"https://radiorecord.hostingradio.ru/rmx96.aacp",
+"record_remix.png",
+"Играет",
+
+"Radio Record Trap",
+"https://radiorecord.hostingradio.ru/trap96.aacp",
+"record_trap.png",
+"Играет",
+
+"Radio Record Lo-Fi",
+"https://radiorecord.hostingradio.ru/lofi96.aacp",
+"record_lofi.png",
+"Играет",
+
+"Radio Record Rock",
+"https://radiorecord.hostingradio.ru/rock96.aacp",
+"record_rock.png",
+"Играет",
+]
--- ver2.js
+++ ver2.js
@@ -31,6 +31,7 @@ $(function () {
     albumName=$("#album-name"),
     trackName=$("#track-name"),
     albumArt=$("#album-art"),
+    idfm=$("#idfm"),
     sArea=$("#s-area"),
     seekBar=$("#seek-bar"),
     trackTime=$("#track-time"),
@@ -48,142 +49,6 @@ $(function () {
     //   "Играет", "Играет", "Играет", "Играет", "Играет",
     //   "Играет", "Играет", "Играет", "Играет", "Играет",
     // ],
-    fm_list=[ // Название Трека
-      "FlyFox",
-      "https://m.ketaru.com:8000/ketaruweb",
-      "img/golive.png",
-      "Играет (Только в Discord)",
-      
-      "TNT Radio",
-      "https://tntradio.hostingradio.ru:8027/tntradio128.mp3",
-      "img/tnt.png",
-      "Играет",
-      
-      "Remix FM",
-      "https://rmx.amgradio.ru/RemixFM",
-      "img/remix.png",
-      "Играет",
-      
-      "DFM",
-      "https://dfm.hostingradio.ru/dfm128.mp3",
-      "img/dfm.png",
-      "Играет",
-      
-      "Like FM",
-      "https://pub0301.101.ru:8443/stream/air/mp3/256/219",
-      "img/like.png",
-      "Играет",
-      
-      "Хайп FM",
-      "https://hfm.amgradio.ru/HypeFM",
-      "img/hype.png",
-      "Играет",
-      
-      "Love Radio",
-      "https://www.liveradio.es/dfmsaransk.sv-veter.ru:8095/love_saransk?128",
-      "img/love.png",
-      "Играет",
-      
-      "EuropaPlus",
-      "https://ep256.hostingradio.ru:8052/europaplus256.mp3",
-      "img/ep.png",
-      "Играет",
-      
-      "EuropaPlus New",
-      "https://europaplus.hostingradio.ru:8014/ep-new128.mp3",
-      "img/ep_new.png",
-      "Играет",
-      
-      "EuropaPlus Lisht",
-      "https://europaplus.hostingradio.ru:8014/ep-light128.mp3",
-      "img/ep_light.png",
-      "Играет",
-      
-      "EuropaPlus Top40",
-      "https://europaplus.hostingradio.ru:8014/ep-top256.mp3",
-      "img/ep_top40.png",
-      "Играет",
-      
-      "Свежее радио",
-      "https://epdop.hostingradio.ru:8033/fresh64.aac",
-      "img/ep_fresh.png",
-      "Играет",
-      
-      "Хит FM",
-      "https://hitfm.hostingradio.ru/hitfm96.aacp",
-      "img/hit.png",
-      "Играет",
-      
-      "Energy FM",
-      "https://pub0102.101.ru:8443/stream/air/mp3/256/99",
-      "img/nrg.png",
-      "Играет",
-      
-      // "Radmir FM",
-      // "http://listen7.myradio24.com/nazarik",
-      // "img/radmir.png",
-      // "Играет",
-      
-      "МУЗ Радио",
-      "https://online-3.gkvr.ru:8001/muztvradio_original_128.mp3", 
-      "img/muz.png",
-      "Играет",
-      
-      "Радио SRP",
-      "https://a7.radioheart.ru:8066/RH6629",
-      "img/srp.png",
-      "Играет",
-      
-      "Relax FM",
-      "https://pub0301.101.ru:8443/stream/air/mp3/256/200",
-      "img/relax.png",
-      "Играет",
-      
-      "Русское Радио",
-      "https://rusradio.hostingradio.ru/rusradio96.aacp",
-      "img/rus.png",
-      "Играет",
-      
-      "Юмор FM",
-      "https://ic5.101.ru:8000/v5_1",
-      "img/umor.png",
-      "Играет",
-      
-      "Новое радио",
-      "https://stream.newradio.ru/novoe96.aacp",
-      "img/newradio.png",
-      "Играет",
-      
-      "Radio Record",
-      "https://radiorecord.hostingradio.ru/rr_main96.aacp",
-      "img/record.png",
-      "Играет",
-      
-      "Radio Record Phonk",
-      "https://radiorecord.hostingradio.ru/phonk96.aacp",
-      "img/record_phonk.png",
-      "Играет",
-      
-      "Radio Record Remix",
-      "https://radiorecord.hostingradio.ru/rmx96.aacp",
-      "img/record_remix.png",
-      "Играет",
-      
-      "Radio Record Trap",
-      "https://radiorecord.hostingradio.ru/trap96.aacp",
-      "img/record_trap.png",
-      "Играет",
-      
-      "Radio Record Lo-Fi",
-      "https://radiorecord.hostingradio.ru/lofi96.aacp",
-      "img/record_lofi.png",
-      "Играет",
-      
-      "Radio Record Rock",
-      "https://radiorecord.hostingradio.ru/rock96.aacp",
-      "img/record_rock.png",
-      "Играет",
-    ],
     playPreviousTrackButton=$("#play-previous"),playNextTrackButton=$("#play-next"),currIndex=-1;
 
   function playPause() {
@@ -192,6 +57,7 @@ $(function () {
       if (audio.paused) {
         playerTrack.addClass("active");
         albumArt.addClass("active");
+        idfm.addClass("active");
         checkBuffering();
         i.attr("class", "fas fa-pause");
         audio.play();
@@ -199,6 +65,7 @@ $(function () {
       } else {
         playerTrack.removeClass("active");
         albumArt.removeClass("active");
+        idfm.removeClass("active");
         clearInterval(buffInterval);
         albumArt.removeClass("buffering");
         i.attr("class", "fas fa-play");
@@ -224,7 +91,6 @@ $(function () {
   }
   function hideHover() {
     sHover.width(0);
-    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
   }
   function playFromClickedPos() {
     audio.currentTime = seekLoc;
@@ -260,10 +126,11 @@ $(function () {
       seekBar.width(0);
       trackTime.removeClass("active");
       
-      currAlbum = currIndex+fm_list[currIndex+Number("0")]; // Radio Name
+      currAlbum = fm_list[currIndex+Number("0")]; // Radio Name
       audio.src = fm_list[currIndex+Number("1")]; // Radio Server
-      currImage = './'+fm_list[currIndex+Number("2")]; // Radio Image
-      currTrackName = fm_list[currIndex+Number("3")]; // Radio Status
+      currImage = fm_list[currIndex+Number("2")]; // Radio Image
+      currTrack = fm_list[currIndex+Number("3")]; // Radio Status
+      currID = (currIndex/4)+1;                       // Radio ID
       
       
       nTime = 0;
@@ -273,6 +140,7 @@ $(function () {
         audio.play();
         playerTrack.addClass("active");
         albumArt.addClass("active");
+        idfm.addClass("active");
         clearInterval(buffInterval);
         checkBuffering();
       }
@@ -280,12 +148,13 @@ $(function () {
         $(this).html(currAlbum).show(200);
       });
       $("#track-name").hide(200, function() {
-        $(this).html(currTrackName).show(200);
+        $(this).html(currTrack).show(200);
       });
       // albumName.text(currAlbum);
-      trackName.text(currTrackName);
-      document.querySelector('img.active').src = currImage;
-      bgArtwork.css({ "background-image": "url(" + currImage + ")" });
+      trackName.text(currTrack);
+      idfm.text(currID);
+      document.querySelector('img.active').src = './img/'+currImage;
+      bgArtwork.css({ "background-image": "url(" + './img/'+currImage + ")" });
     } else {
       if (flag == 0 || flag == 1) currIndex++;
       else ++currIndex;
@@ -303,10 +172,18 @@ $(function () {
     sArea.on("click", playFromClickedPos);
     $(audio).on("timeupdate", updateCurrTime);
     playPreviousTrackButton.on("click", function () {
-      selectTrack(-1);
+      let i = 0;
+      for (; i < 4; i++) {
+        selectTrack(-1);
+      }
     });
     playNextTrackButton.on("click", function () {
-      selectTrack(1);
+      let i = 0;
+      let g = 4;
+      if (currIndex <= fm_list.length-(g*2)){
+        for (; i < g; i++) {
+          selectTrack(1);
+        }};
     });
   }
   initPlayer();
